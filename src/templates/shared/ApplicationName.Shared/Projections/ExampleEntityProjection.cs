129ff7c3e95452fadcba94eebf364e49a094eb55
ftechmax <29160411+ftechmax@users.noreply.github.com>
2024-05-07 11:47:04 +0200

    add projection classes

---
+++ src/templates/shared/ApplicationName.Shared/Projections/ExampleEntityProjection.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationName.Shared.Aggregates;
+using ProtoBuf;
+
+namespace ApplicationName.Shared.Projections;
+
+[ProtoContract]
+public record ExampleEntityProjection : IExampleEntity
+{
+    [ProtoMember(1)]
+    public Guid Id { get; init; }
+
+    [ProtoMember(2)]
+    public string Name { get; init; }
+
+    [ProtoMember(3)]
+    public float SomeValue { get; init; }
+}
---
+++ src/templates/shared/ApplicationName.Shared/Projections/ExampleProjection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using ApplicationName.Shared.Aggregates;
+using ProtoBuf;
+
+namespace ApplicationName.Shared.Projections;
+
+[ProtoContract]
+public record ExampleProjection : IExample
+{
+    [ProtoMember(1)]
+    public Guid Id { get; init; }
+
+    [ProtoMember(2)]
+    public DateTime Created { get; init; }
+
+    [ProtoMember(3)]
+    public DateTime Updated { get; init; }
+
+    [ProtoMember(4)]
+    public string Name { get; init; }
+
+    [ProtoMember(5)]
+    public string Description { get; init; }
+
+    [ProtoMember(6)]
+    public Collection<ExampleEntityProjection> Examples { get; init; } = [];
+
+    [ProtoMember(7)]
+    public ExampleValueObjectProjection ExampleValueObject { get; init; }
+
+    [ProtoMember(8)]
+    public int? RemoteCode { get; init; }
+
+    IReadOnlyCollection<IExampleEntity> IExample.Examples => Examples;
+
+    IExampleValueObject IExample.ExampleValueObject => ExampleValueObject;
+}
